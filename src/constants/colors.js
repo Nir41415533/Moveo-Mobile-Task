663cb28cc2c3d2f862f2e53bc5cf3dbb225d35ce
Nir Ohayon <nir41415511@gmail.com>
2026-02-21 13:35:24 +0200

    style: update colors constants

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,9 +1,9 @@
 export const COLORS = {
-    white: '#ffffff',      // רקע נקי
-    lightBlue: '#67719d',  // טקסט משני או אייקונים
-    mediumBlue: '#424769', // כפתורים משניים
-    darkBlue: '#2d3250',   // צבע ראשי (Header / Background)
-    accent: '#f9b17a',     // צבע פעולה (כפתור שמירה, נעצים במפה, FAB)
+    white: '#ffffff',      
+    lightBlue: '#67719d',  
+    mediumBlue: '#424769', 
+    darkBlue: '#2d3250',   
+    accent: '#f9b17a',   
     red: '#ff6b6b'
 
   };
